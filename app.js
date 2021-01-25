75c1df7fd5f8662600f3859fb8b9e3eae1a19d80
soonniee <ckstns1231@gmail.com>
2021-01-25 23:35:08 +0900

    2.1 2D Context

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
 const cavas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext('2d');
+
+canvas.width = 700;
+canvas.height = 700;
+
+ctx.strokeStyle = "black";
+ctx.lineWidth = 2.5;
 let painting = false;
 
 function stopPainting(){
     painting=false;
 }
+
+function startPainting(){
+    painting=true;
+}
 function onMouseMove(event){
     const x = event.offsetX;
     const y = event.offsetY;
-    console.log(event);
+    if(!painting){
+        ctx.beginPath();
+        ctx.moveTo(x,y);
+    }else{
+        ctx.lineTo(x,y);
+        ctx.stroke();
+    }
 }
 
 function onMouseDown(event){
@@ -22,7 +39,7 @@ function onMouseLeave(event){
 }
 if(canvas){
     canvas.addEventListener("mousemove",onMouseMove);
-    canvas.addEventListener("mousedonw",onMouseDown);
-    canvas.addEventListener("mouseup",onMouseUp);
+    canvas.addEventListener("mousedonw",startPainting);
+    canvas.addEventListener("mouseup",stopPainting);
     canvas.addEventListener("mouseleave",stopPainting);
 }
