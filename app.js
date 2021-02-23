b565ddc1ae13d117d53d758c45a644d81d0da383
soonniee <ckstns1231@gmail.com>
2021-02-24 00:21:19 +0900

    #2.1 2D Context / Changing Colors

--- app.js
+++ app.js
@@ -1,28 +1,48 @@
-const cavas = document.getElementById("jsCanvas");
+const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jsColor")
+canvas.width = 700;
+canvas.height = 700;
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
+
 let painting = false;
 
 function stopPainting(){
     painting=false;
 }
+function startPainting(){
+    painting = true;
+}
 function onMouseMove(event){
     const x = event.offsetX;
     const y = event.offsetY;
-    console.log(event);
+    if(!painting){
+        ctx.beginPath();
+        ctx.moveTo(x,y);
+    }else{
+        ctx.lineTo(x,y);
+        ctx.stroke();
+    }
 }
 
 function onMouseDown(event){
     painting=true;
-
 }
 function onMouseUp(event){
-    stopPainting()
+    stopPainting();
 }
 function onMouseLeave(event){
     painting=false;
 }
+function handleColorClicked(event){
+    const color = evnet.target.style.backgroundColor;
+    ctx.strokeStyle = color;
+}
 if(canvas){
     canvas.addEventListener("mousemove",onMouseMove);
-    canvas.addEventListener("mousedonw",onMouseDown);
+    canvas.addEventListener("mousedown",startPainting);
     canvas.addEventListener("mouseup",onMouseUp);
     canvas.addEventListener("mouseleave",stopPainting);
 }
+Array.from(colors).forEach(color=>color.addEventListener("click",handleColorClicked));
